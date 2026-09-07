d2512a845129615b351ebb302489b0655982cbf5
samriddha-gautam <samridgautam@gmail.com>
2026-09-07 21:29:51 +0545

    react-hooks: useState

--- myapp/src/components/Button.tsx
+++ myapp/src/components/Button.tsx
@@ -2,11 +2,13 @@ type ButtonProps={
     text : string ;
     onclick ?: ()=>void;
     variant : "primary" | "secondary" | "danger" ;
+    type ?: "button" | "submit" | "reset"
 }
 
-export default function Button ({text ,onclick , variant = "primary" } : ButtonProps){
+export default function Button ({text ,onclick ,type = "button" , variant = "primary" } : ButtonProps){
     return (
         <button 
+            type={type}
             onClick={onclick}
             className={`font-bold cursor-pointer p-2 rounded-xl hover:opacity-75 text-white ${variant === "primary" ? 'bg-green-400 text-white' : variant === "secondary" ? 'bg-yellow-500 text-white' : 'bg-red-600 text-white' } `}
         >
