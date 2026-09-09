d2ca7697f687e816052a882401fd436d3b75f343
samriddha-gautam <samridgautam@gmail.com>
2026-09-09 22:04:29 +0545

    todo application using useeffect api call

--- myapp/src/components/Card.tsx
+++ myapp/src/components/Card.tsx
@@ -1,7 +1,7 @@
-import Button from "./Button";
 
 type CardProps = {
     entity: {
+        extraproperty ?: any
         id: number
         name: string
         }
@@ -13,8 +13,9 @@ type CardProps = {
 export default function Card({ entity, boolValue = false }: CardProps) {
     return (
         <div>
+            <h2 className="font-bold text-5xl text-black">{entity.extraproperty}</h2>
             <h3 className="font-bold text-2xl text-blue-500">{entity.name}</h3>
-            <p className={`${boolValue ? 'text-green-400' : 'text-red-400'}`}>{boolValue ? "✅ Online" : "❌ Offline"}</p>
+            <p className={`${boolValue ? 'text-green-400' : 'text-red-400'}`}>{boolValue ? "✅ " : "❌ "}</p>
 
             {/* <ConditionalButtonRendering boolValue = {boolValue}/> */}
 
