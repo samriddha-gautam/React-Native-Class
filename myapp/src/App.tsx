4c668c6067157580aa26adcb31fc3b381ab9632f
samriddha-gautam <samridgautam@gmail.com>
2026-09-09 21:45:35 +0545

    useEffect and json data fetching

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,59 +1,45 @@
-import { useState } from "react";
-import Button from "./components/Button";
-import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
-type Task = {
+type User = {
   id: number;
-  title: string;
-  completed: boolean;
-  status: "completed" | "in-progress" | "failed"
+  name: string;
+  email: string;
 };
 
-const initialTasks: Task[] = [
-  { id: 1, title: "Read", completed: true , status:"completed"},
-  { id: 2, title: "Code", completed: false , status:"in-progress"},
-  { id: 3, title: "Play", completed: false , status: "completed"},
-];
-
 export default function App() {
-    
-    const [tasks, setTasks] = useState<Task[]>(initialTasks)
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    const toggleTaskComplete = (id:number) => {
-        
-        setTasks(tasks.map((task)=>task.id === id? {...task , completed:!task.completed}:task))
-  }
-    
-    function completedTasks({status}: {status :"completed" | "in-progress" | "failed"}){
-        if(status === "completed")
-            return <p className="text-green-400"><b>The task has been completed</b></p>
-        if(status=== "in-progress")
-            return <p className="text-yellow-500"><b>The task is in progres ...</b></p>
-        return <p className="text-red-600"><b>Failed to do the task!</b></p>
+  const [count , setCount] = useState<number>(0)
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+        );
+        const data: User[] = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError("Failed to load users");
+      } finally {
+        setIsLoading(false);
+      }
     }
+    fetchUsers();
+  },[count]);
+  if (isLoading) return <p>Loading user data...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div>
-      {tasks.map((task) => (
-        <div  className="bg-yellow-200 w-67  m-8 p-4 rounded-xl mx-auto">
-          <Card
-            key={task.id}
-            entity={{ id: task.id, name: task.title }}
-            boolValue={task.completed}
-          />
-          <Button
-            key={task.id}
-            text={task.completed ? "Done" : "Mark Cmplete"}
-            onclick={()=>toggleTaskComplete(task.id)}
-            variant={task.completed ? "primary" : "secondary"}
-          />
-        <div key={task.id}>
-            {completedTasks({status:task.status})}
-        </div>
-        </div>
-        
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.name} - {user.email}
+        </li>
       ))}
-     
-      
-    </div>
+    <button className="cursor-pointer" onClick={()=>setCount(count+1)}>Click {count}</button>
+    </ul>
+    
   );
 }
