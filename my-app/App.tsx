8e2e4f4392b5e3a18f09e64b9c919705002a1c51
samriddha-gautam <samridgautam@gmail.com>
2026-09-24 21:06:41 +0545

    Stylesheet react native

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,46 +1,93 @@
-import { View, Text, ScrollView } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-
-type Skill = {
-  id: number
-  name: string;
-};
-
-const skills: Skill[] = [
-  {
-    id: 1,
-    name: "Typescripts",
-  },
-  {
-    id: 2,
-    name: "React",
-  },
-  {
-    id: 3,
-    name: "React Native",
-  },
-];
+import { Platform, View, Text, StyleSheet, Dimensions } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaProvider style={{flex:1, margin:10}}>
-      <ScrollView>
-        {/* Block-1 */}
-        <View>
-          <Text>Samriddha Gautam</Text>
-          <Text>Mobile Developer</Text>
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Home</Text>
+        <Text style={styles.headerText}>Contact</Text>
+        <Text style={styles.headerText}>About</Text>
+      </View>
+      <View style={styles.row}>
+        <View style={[styles.container]}>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>1</Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>2</Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>3</Text>
+          </View>
         </View>
-        <View>
-          <Text>Skills:</Text>
-          {skills.map((skill) => (
-            <View key={skill.id}>
-              <Text> - {skill.name}</Text>
-            </View>
-          ))}
+        <View style={styles.container2}>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>a</Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>b</Text>
+          </View>
+          <View style={styles.box}>
+            <Text style={styles.viewText}>c</Text>
+          </View>
         </View>
-        
-      </ScrollView>
-    </SafeAreaProvider>
+      </View>
+      <View style={styles.card}>
+        <Text style={styles.viewText}>This is the card contents</Text>
+      </View>
+    </View>
   );
 }
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: Platform.OS === "android" ? 44 : 24,
+    flexDirection:"row",
+    justifyContent:'space-evenly'
+  },
+  headerText:{
+    fontWeight:900,
+    fontSize:21,
+    color:'red'
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+  },
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  container2: {
+    flex: 1,
+    marginTop: 50,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: "steelblue",
+    margin: 5,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewText: {
+    color: "#ffffff",
+    fontWeight: "900",
+    fontSize: 24,
+  },
+  card: {
+    width: screenWidth * 0.9,
+    height: "50%",
+    borderRadius: 15,
+    padding: 16,
+    backgroundColor: "green",
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
